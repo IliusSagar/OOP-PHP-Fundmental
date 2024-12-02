36eccec314ecb7dfdf30edb6d7b4c229d6d5771b
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-03 00:10:41 +0600

    Getter & Setter Method

--- Classes/Car.php
+++ Classes/Car.php
@@ -4,13 +4,43 @@ class Car {
     // Properties / Fields
     private $brand;
     private $color;
-    public $vehicleType = "car";
+    // public $vehicleType = "car";
 
     // Constructor
     public function __construct($brand, $color = "none")
     {
         $this->brand = $brand;
         $this->color = $color;
+    }
+
+    // Getter & Setter Methods
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $allowedColors = [
+            "red",
+            "blue",
+            "green",
+            "yellow"
+        ];
+        if(in_array($color, $allowedColors)){
+            $this->color = $color;
+        }
+     
     }
 
     // Method
--- index.php
+++ index.php
@@ -11,7 +11,10 @@
         require_once 'Classes/Car.php';
 
         $car01 = new Car("BMW", "green");
-        echo $car01->vehicleType;
+        // echo $car01->setBrand("Volvo");
+
+        $car01->setColor("white");
+        echo $car01->getColor();
     ?>
     
 </body>
